8ba1ef5efe2cfce2ce45577ad2dda53b7a957a53
James McC <mariobud@gmail.com>
2020-04-25 16:12:44 -0500

    Two-way binding and Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoursesComponents } from './courses.component';
-import { CourseComponent } from './course/course.component';
-import { CoursesService } from './courses.services';
 import { AuthorComponent } from './author/author.component';
 import { AuthorService } from './author.service';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponents,
-    AuthorComponent
+    AuthorComponent,
+    SummaryPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [CoursesService, AuthorService],
+  providers: [AuthorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,29 +1,38 @@
 import {Component} from '@angular/core';
-import { CoursesService } from './courses.services';
 
 
 @Component({
     selector: 'courses', //<courses>
-    template: `<h2>{{ getTitle() }}</h2>
-    <li *ngFor="let course of courses">
-    {{course}}
-</li>
-    <table>
-        <tr>
-            <td [attr.colspan]="colSpan"></td>
-        </tr>
-    </table>
+    template: `
+    {{course.title | uppercase}} <br/>
+    {{course.students | number}} <br/>
+    {{course.rating | number:'1.2-2' }} <br/>
+    {{course.price | currency:'AUD':true:'3.2-2' }} <br/>
+    {{course.releaseDate | date:'shortDate' }} <br/>
+    {{ text | summary:10 }}
+
     <div (click) = onDivClicked()>
     <button (click) = "onSave($event)" class="btn btn-primary" [style.backgroundColor] ="isActive ? 'blue' : 'white'">Save</button>
     </div>
-    <input (keyup.enter)="onKeyUp()"/>
+    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+    <div>
+    {{ course.title}} <br/>
+    </div>
     `
 })
 export class CoursesComponents {
     title = "List of courses";
     colSpan = 2;
     isActive = true;
-    courses;
+    course = {
+        title: "The Complete Course",
+        rating: 4.321,
+        students: 321,
+        price: 109.32,
+        releaseDate: new Date(2016,3,1)
+    }
+    email = "me@example.com";
+    text = 'fkjbrnthkjbnhtrelnhretlnrehlnkrelknreblkngreklngrkngrem grm,gre,mgrjgrenlkgrel.grel gr;jng; r;nrqg;k rgqe;knrge;kngre;kngr;kn;kn;knr;knfk;nhf khtr. mhrtknl htrk lhtrknlhtrklnhtklnrklnrthklnhtlkntrhnkhtrklklnhtrnklthk;nterh;kner;knethk;nekn;hre;nkr;nr;kn;nkirh;knrehgk;n'
 
     onDivClicked(){
         console.log("Div was clicked");
@@ -31,11 +40,6 @@ export class CoursesComponents {
 
     getTitle() {
         return this.title;
-    }
-    
-
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
     }
 
     onSave($event){
@@ -45,6 +49,6 @@ export class CoursesComponents {
     }
 
     onKeyUp(){
-        console.log("Enter is pressed");
+        console.log(this.email);
     }
 }
