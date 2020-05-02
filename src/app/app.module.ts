ebb6d4adc34d29980df440f0073470279d7ba792
James McC <mariobud@gmail.com>
2020-05-02 14:16:34 -0500

    Favorite Icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { CoursesComponents } from './courses.component';
 import { AuthorComponent } from './author/author.component';
 import { AuthorService } from './author.service';
 import { SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponents,
     AuthorComponent,
-    SummaryPipe
+    SummaryPipe,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
